a7eaaa7f2beae4b109ff63743f2911477f5dde71
locknes <locknes91@windowslive.com>
2011-07-28 23:38:56 +0200

    Add fixes from the tracker close #128 #127 #122 #117

---
+++ sql/ChaosCore/v4_world_fixes_from_tracker.sql
@@ -0,0 +1,8 @@
+INSERT INTO areatrigger_teleport (id ,name ,target_map ,target_position_x ,target_position_y ,target_position_z ,target_orientation)
+VALUES
+('45', 'Scarlet Monastery - Graveyard (Entrance)', '189', '1688.99', '1053.48', '18.6775', '0.00117');
+INSERT INTO spell_linked_spell VALUES
+(-69674, 69706, 0, 'Rotface: Mutated Infection Summon'),
+(-71224, 69706, 0, 'Rotface: Mutated Infection Summon'),
+(-73022, 69706, 0, 'Rotface: Mutated Infection Summon'),
+(-73023, 69706, 0, 'Rotface: Mutated Infection Summon');
