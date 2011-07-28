eae0c0bd70a737319b741dc9bbaa20715a2c42d4
Ruben Motta Mussi de Souza <rubenmussi@hotmail.com>
2011-07-27 23:21:20 -0300

    Lower ICC area buff benefit (i'll raise it on the next months)

---
+++ sql/ChaosCore/v4_world_icc_area_aura_benefit.sql
@@ -0,0 +1,3 @@
+-- Lower ICC aura benefit
+UPDATE `spell_area` SET `spell` = 73816 WHERE `spell` = 73822; -- Hellscreams warsong
+UPDATE `spell_area` SET `spell` = 73762 WHERE `spell` = 73828; -- Strength of Wrynn
