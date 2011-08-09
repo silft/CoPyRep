silft/CoPyRep

commit a5997ea14102d4ef98313cedd52cad74eaafe243

Fix Bladestorm slow issue on periodic AoE triggered movement impairing effect auras i.e. desecration,frost trap. Also added a SQL workaround to Hex, Mind control, and Cyclone immunities.
AoE part patch by Dr. Tenma

Note: Tenma's patch is very old, it was just ported to newer rev, so a proper feedback is needed to know if it didn't broke anything.

src/server/game/Spells/Auras/SpellAuras.cpp
@@ -207,6 +207,33 @@ void AuraApplication::BuildUpdatePacket(ByteBuffer& data, bool remove) const
     }
 }
 
+void AuraApplication::HandleEffect(uint8 effIndex, bool apply)
+{
+    if (GetRemoveMode() || GetBase()->IsRemoved())
+        return;
+
+    if (!GetBase()->HasEffect(effIndex))
+    {
+        sLog->outError("AuraApplication::HandleEffect: tried to %s nonexisting aura effect - id: %u effIndex: %u", (apply ? "APPLY" : "UNNAPLY"), GetBase()->GetSpellProto()->Id, effIndex);
+        return;
+    }
+
+    if (!GetTarget())
+    {
+        sLog->outError("AuraApplication::HandleEffect: target is NULL at %s - id: %u effIndex: %u", (apply ? "APPLY" : "UNNAPLY"), GetBase()->GetSpellProto()->Id, effIndex);
+        return;
+    }
+    else if (!GetTarget()->isAlive())
+        return;
+
+    if (apply ? HasEffect(effIndex) : !HasEffect(effIndex))
+        return;
+
+    m_effectsToApply |= (1 << effIndex);
+
+    _HandleEffect(effIndex, apply);
+}
+
 void AuraApplication::ClientUpdate(bool remove)
 {
     m_needClientUpdate = false;
@@ -473,9 +500,10 @@ void Aura::UpdateTargetMap(Unit* caster, bool apply)
     FillTargetMap(targets, caster);
 
     UnitList targetsToRemove;
+	std::map<Unit *, std::pair<AuraApplication *, uint8> > targetsToReapply;
 
     // mark all auras as ready to remove
-    for (ApplicationMap::iterator appIter = m_applications.begin(); appIter != m_applications.end();++appIter)
+    for (ApplicationMap::iterator appIter = m_applications.begin(); appIter != m_applications.end(); ++appIter)
     {
         std::map<Unit *, uint8>::iterator existing = targets.find(appIter->second->GetTarget());
         // not found in current area - remove the aura
@@ -483,10 +511,25 @@ void Aura::UpdateTargetMap(Unit* caster, bool apply)
             targetsToRemove.push_back(appIter->second->GetTarget());
         else
         {
+            if (!CanBeAppliedOn(existing->first))
+                targetsToRemove.push_back(appIter->second->GetTarget());
+            else
+            {
+                // Reapply disabled auraeffects
+                uint8 effReapply = 0;
+                for (uint32 effIndex = 0; effIndex < MAX_SPELL_EFFECTS; ++effIndex)
+                    if (existing->second & (1 << effIndex))
+                        if ((GetSpellProto()->Effect[effIndex] != SPELL_EFFECT_APPLY_AURA) &&
+                            (GetSpellProto()->EffectMechanic[effIndex]) &&
+                            (!appIter->second->GetTarget()->IsImmunedToSpellEffect(GetSpellProto(), effIndex)))
+                            effReapply |= (1 << effIndex);
+                if (effReapply)
+                    targetsToReapply[appIter->second->GetTarget()] = std::make_pair<AuraApplication *, uint8>(appIter->second, effReapply);
+            }
             // needs readding - remove now, will be applied in next update cycle
             // (dbcs do not have auras which apply on same type of targets but have different radius, so this is not really needed)
-            if (appIter->second->GetEffectMask() != existing->second || !CanBeAppliedOn(existing->first))
-                targetsToRemove.push_back(appIter->second->GetTarget());
+            //if (appIter->second->GetEffectMask() != existing->second || !CanBeAppliedOn(existing->first))
+            //    targetsToRemove.push_back(appIter->second->GetTarget());
             // nothing todo - aura already applied
             // remove from auras to register list
             targets.erase(existing);
@@ -519,9 +562,19 @@ void Aura::UpdateTargetMap(Unit* caster, bool apply)
 
         bool addUnit = true;
         // check target immunities
-        if (itr->first->IsImmunedToSpell(GetSpellProto())
-            || !CanBeAppliedOn(itr->first))
+        if (itr->first->IsImmunedToSpell(GetSpellProto()) || !CanBeAppliedOn(itr->first))
             addUnit = false;
+        else
+        {
+            for (uint32 effIndex = 0; effIndex < MAX_SPELL_EFFECTS; ++effIndex)
+                if (itr->second & (1 << effIndex))
+                    if ((GetSpellProto()->EffectMechanic[effIndex]) &&
+                        (itr->first->IsImmunedToSpellEffect(GetSpellProto(), effIndex)))
+                        itr->second &= ~(1 << effIndex);
+
+            if (!itr->second)
+                addUnit = false;
+        }
 
         if (addUnit)
         {
@@ -571,13 +624,28 @@ void Aura::UpdateTargetMap(Unit* caster, bool apply)
         return;
 
     // apply aura effects for units
-    for (std::map<Unit *, uint8>::iterator itr = targets.begin(); itr!= targets.end();++itr)
+    for (std::map<Unit *, uint8>::iterator itr = targets.begin(); itr != targets.end(); ++itr)
     {
         if (AuraApplication * aurApp = GetApplicationOfTarget(itr->first->GetGUID()))
         {
             // owner has to be in world, or effect has to be applied to self
             ASSERT((!GetOwner()->IsInWorld() && GetOwner() == itr->first) || GetOwner()->IsInMap(itr->first));
             itr->first->_ApplyAura(aurApp, itr->second);
+        }
+    }
+    // reapply aura effects for units
+    for (std::map<Unit *, std::pair<AuraApplication *, uint8> >::iterator itr = targetsToReapply.begin(); itr != targetsToReapply.end(); ++itr)
+    {
+        if (AuraApplication * aurApp = GetApplicationOfTarget(itr->first->GetGUID()))
+        {
+            if (itr->second.first != aurApp)
+                continue;
+
+            // owner has to be in world, or effect has to be applied to self
+            ASSERT((!GetOwner()->IsInWorld() && GetOwner() == itr->first) || GetOwner()->IsInMap(itr->first));
+            for (uint32 effIndex = 0; effIndex < MAX_SPELL_EFFECTS; ++effIndex)
+                if (itr->second.second & (1 << effIndex))
+                    aurApp->HandleEffect(effIndex, true);
         }
     }
 }
@@ -2250,6 +2318,7 @@ void UnitAura::FillTargetMap(std::map<Unit *, uint8> & targets, Unit* caster)
     {
         if (!HasEffect(effIndex))
             continue;
+
         UnitList targetList;
         // non-area aura
         if (GetSpellProto()->Effect[effIndex] == SPELL_EFFECT_APPLY_AURA)
@@ -2345,9 +2414,10 @@ void DynObjAura::FillTargetMap(std::map<Unit *, uint8> & targets, Unit* /*caster
     {
         if (!HasEffect(effIndex))
             continue;
+
         UnitList targetList;
-        if (GetSpellProto()->EffectImplicitTargetB[effIndex] == TARGET_DEST_DYNOBJ_ALLY
-            || GetSpellProto()->EffectImplicitTargetB[effIndex] == TARGET_UNIT_AREA_ALLY_DST)
+        if (GetSpellProto()->EffectImplicitTargetB[effIndex] == TARGET_DEST_DYNOBJ_ALLY ||
+            GetSpellProto()->EffectImplicitTargetB[effIndex] == TARGET_UNIT_AREA_ALLY_DST)
         {
             Trinity::AnyFriendlyUnitInObjectRangeCheck u_check(GetDynobjOwner(), dynObjOwnerCaster, radius);
             Trinity::UnitListSearcher<Trinity::AnyFriendlyUnitInObjectRangeCheck> searcher(GetDynobjOwner(), targetList, u_check);

sql/ChaosCore/v4_world_bladestorm.sql
@@ -1,0 +1,5 @@
+DELETE FROM spell_linked_spell WHERE `spell_trigger` = 46924;
+INSERT INTO spell_linked_spell VALUES
+(46924,-51514,2,'Bladestorm Immunity to Hex'),
+(46924,-605,2,'Bladestorm Immunity to Mind Control'),
+(46924,-33786,2,'Bladestorm Immunity to Cyclone');

src/server/game/Entities/Unit/Unit.cpp
@@ -4041,7 +4041,7 @@ void Unit::RemoveAurasWithFamily(SpellFamilyNames family, uint32 familyFlag1, ui
 
 void Unit::RemoveMovementImpairingAuras()
 {
-    RemoveAurasWithMechanic((1<<MECHANIC_SNARE)|(1<<MECHANIC_ROOT));
+    HandleAuraEffectsWithMechanic(false, (1 << MECHANIC_SNARE) | (1 << MECHANIC_ROOT));
 }
 
 void Unit::RemoveAurasWithMechanic(uint32 mechanic_mask, AuraRemoveMode removemode, uint32 except)
@@ -4189,6 +4189,54 @@ void Unit::RemoveAllAurasExceptType(AuraType type)
             RemoveOwnedAura(iter, AURA_REMOVE_BY_DEFAULT);
         else
             ++iter;
+    }
+}
+
+void Unit::HandleAuraEffectsWithMechanic(bool apply, uint32 mechanic_mask, AuraRemoveMode removemode, uint32 except)
+{
+    if (apply)
+    {
+        for (AuraApplicationMap::iterator iter = m_appliedAuras.begin(); iter != m_appliedAuras.end(); ++iter)
+        {
+            Aura const * aura = iter->second->GetBase();
+            if (!except || aura->GetId() != except)
+                for (int32 effIndex = 0; effIndex < MAX_SPELL_EFFECTS; ++effIndex)
+                    if ((aura->GetSpellProto()->Effect[effIndex] != SPELL_EFFECT_APPLY_AURA) &&
+                        (mechanic_mask & (1 << aura->GetSpellProto()->EffectMechanic[effIndex])))
+                        iter->second->HandleEffect(effIndex, true);
+        }
+        return;
+    }
+    else
+    {
+        for (AuraApplicationMap::iterator iter = m_appliedAuras.begin(); iter != m_appliedAuras.end();)
+        {
+            Aura const * aura = iter->second->GetBase();
+            if (!except || aura->GetId() != except)
+            {
+                if ((1 << aura->GetSpellProto()->Mechanic) & mechanic_mask)
+                {
+                    RemoveAura(iter, removemode);
+                    continue;
+                }
+                bool cont = false;
+                for (int32 effIndex = 0; effIndex < MAX_SPELL_EFFECTS; ++effIndex)
+                    if (mechanic_mask & (1 << aura->GetSpellProto()->EffectMechanic[effIndex]))
+                    {
+                        iter->second->HandleEffect(effIndex, false);
+                        if (!iter->second->GetEffectMask())
+                        {
+                            RemoveAura(iter, removemode);
+                            cont = true;
+                            break;
+                        }
+                    }
+                if (cont)
+                    continue;
+            }
+            ++iter;
+        }
+        return;
     }
 }
 
@@ -5545,7 +5593,7 @@ bool Unit::HandleDummyAuraProc(Unit* victim, uint32 damage, AuraEffect* triggere
                         return false;
 
                     // Remove any stun effect on target
-                    victim->RemoveAurasWithMechanic(1<<MECHANIC_STUN, AURA_REMOVE_BY_ENEMY_SPELL);
+                    victim->HandleAuraEffectsWithMechanic(false, (1 << MECHANIC_STUN), AURA_REMOVE_BY_ENEMY_SPELL);
                     return true;
                 }
                 // Glyph of Scourge Strike
@@ -7748,7 +7796,7 @@ bool Unit::HandleDummyAuraProc(Unit* victim, uint32 damage, AuraEffect* triggere
                     case 49920: triggered_spell_id = 66991; break;                            // Rank 5
                     case 49921: triggered_spell_id = 66992; break;                            // Rank 6
 
-                    // Death Strike
+					// Death Strike
                     case 49998: triggered_spell_id = 66188; break;                            // Rank 1
                     case 49999: triggered_spell_id = 66950; break;                            // Rank 2
                     case 45463: triggered_spell_id = 66951; break;                            // Rank 3
@@ -11729,6 +11777,7 @@ bool Unit::IsImmunedToSpellEffect(SpellEntry const* spellInfo, uint32 index) con
 {
     if (!spellInfo)
         return false;
+
     // If m_immuneToEffect type contain this effect type, IMMUNE effect.
     uint32 effect = spellInfo->Effect[index];
     SpellImmuneList const& effectList = m_spellImmune[IMMUNITY_EFFECT];
@@ -11753,9 +11802,9 @@ bool Unit::IsImmunedToSpellEffect(SpellEntry const* spellInfo, uint32 index) con
         // Check for immune to application of harmful magical effects
         AuraEffectList const& immuneAuraApply = GetAuraEffectsByType(SPELL_AURA_MOD_IMMUNE_AURA_APPLY_SCHOOL);
         for (AuraEffectList::const_iterator iter = immuneAuraApply.begin(); iter != immuneAuraApply.end(); ++iter)
-            if (spellInfo->Dispel == DISPEL_MAGIC &&                                      // Magic debuff
-                ((*iter)->GetMiscValue() & GetSpellSchoolMask(spellInfo)) &&  // Check school
-                !IsPositiveEffect(spellInfo->Id, index))                                  // Harmful
+            if ((spellInfo->Dispel == DISPEL_MAGIC) &&                              // Magic debuff
+                ((*iter)->GetMiscValue() & GetSpellSchoolMask(spellInfo)) &&        // Check school
+                !IsPositiveEffect(spellInfo->Id, index))                            // Harmful
                 return true;
     }
 
@@ -16497,13 +16546,14 @@ Aura* Unit::AddAura(SpellEntry const* spellInfo, uint8 effMask, Unit* target)
     if (target->IsImmunedToSpell(spellInfo))
         return NULL;
 
-    for (uint32 i = 0; i < MAX_SPELL_EFFECTS; ++i)
+    for (uint32 effIndex = 0; effIndex < MAX_SPELL_EFFECTS; ++effIndex)
     {
-        if (!(effMask & (1<<i)))
+        if (!(effMask & (1 << effIndex)))
             continue;
-        if (target->IsImmunedToSpellEffect(spellInfo, i))
-            effMask &= ~(1<<i);
-    }
+        if ((spellInfo->Effect[effIndex] == SPELL_EFFECT_APPLY_AURA) &&
+            (target->IsImmunedToSpellEffect(spellInfo, effIndex)))
+            effMask &= ~(1 << effIndex);
+    }  
 
     if (Aura* aura = Aura::TryRefreshStackOrCreate(spellInfo, effMask, target, this))
     {

src/server/game/Entities/Unit/Unit.h
@@ -1708,6 +1708,7 @@ class Unit : public WorldObject
         void RemoveAurasWithAttribute(uint32 flags);
         void RemoveAurasWithFamily(SpellFamilyNames family, uint32 familyFlag1, uint32 familyFlag2, uint32 familyFlag3, uint64 casterGUID);
         void RemoveAurasWithMechanic(uint32 mechanic_mask, AuraRemoveMode removemode = AURA_REMOVE_BY_DEFAULT, uint32 except=0);
+		void HandleAuraEffectsWithMechanic(bool apply, uint32 mechanic_mask, AuraRemoveMode removemode = AURA_REMOVE_BY_DEFAULT, uint32 except=0);
         void RemoveMovementImpairingAuras();
 
         void RemoveAreaAurasDueToLeaveWorld();

src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -3430,7 +3430,9 @@ void AuraEffect::HandleModMechanicImmunity(AuraApplication const* aurApp, uint8 
     }
 
     if (apply && GetSpellProto()->AttributesEx & SPELL_ATTR1_DISPEL_AURAS_ON_IMMUNITY)
-        target->RemoveAurasWithMechanic(mechanic, AURA_REMOVE_BY_DEFAULT, GetId());
+        target->HandleAuraEffectsWithMechanic(false, mechanic, AURA_REMOVE_BY_DEFAULT, GetId());
+    else
+        target->HandleAuraEffectsWithMechanic(true, mechanic, AURA_REMOVE_NONE, GetId());
 }
 
 void AuraEffect::HandleAuraModEffectImmunity(AuraApplication const* aurApp, uint8 mode, bool apply) const

src/server/game/Spells/Auras/SpellAuras.h
@@ -70,6 +70,7 @@ class AuraApplication
         bool IsPositive() const { return m_flags & AFLAG_POSITIVE; }
         bool IsSelfcasted() const { return m_flags & AFLAG_CASTER; }
         uint8 GetEffectsToApply() const { return m_effectsToApply; }
+		void HandleEffect(uint8 effIndex, bool apply);
 
         void SetRemoveMode(AuraRemoveMode mode) { m_removeMode = mode; }
         AuraRemoveMode GetRemoveMode() const {return m_removeMode;}

commit 119de2b156d258bc9a9563f1a71abd7a7d82beea

Fix crash on festergut caused by a5997ea14102d4ef98313cedd52cad74eaafe243

src/server/game/Spells/Auras/SpellAuras.cpp
@@ -209,6 +209,9 @@ void AuraApplication::BuildUpdatePacket(ByteBuffer& data, bool remove) const
 
 void AuraApplication::HandleEffect(uint8 effIndex, bool apply)
 {
+	if (!effIndex || !apply)
+		return;
+
     if (GetRemoveMode() || GetBase()->IsRemoved())
         return;
 