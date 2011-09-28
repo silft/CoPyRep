ebef3ea046f1e4596f59b899351dff23490b85aa
Ruben Motta Mussi de Souza <rubenmussi@hotmail.com>
2011-09-28 17:32:34 -0300

    Core/lol: rofl, one more typo (i caught a cold, so dount blame me, lol)

--- src/server/scripts/Northrend/IcecrownCitadel/boss_professor_putricide.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_professor_putricide.cpp
@@ -701,7 +701,7 @@ class npc_volatile_ooze : public CreatureScript
                 if (victim && victim->IsWithinDistInMap(me, 1.0f) && victim->HasAura(SPELL_VOLATILE_OOZE_ADHESIVE) && victim->isAlive())
                 {
                     DoCast(me, SPELL_OOZE_ERUPTION);
-                    victim->RemoveAura(SPELL_VOLATILE_OOZE_ADHESIVE,AURA_REMOVE_BY_ENEMY_SPELL);
+                    victim->RemoveAurasDueToSpell(SPELL_VOLATILE_OOZE_ADHESIVE, 0, 0, AURA_REMOVE_BY_ENEMY_SPELL);
                 }
 
                 if (!_newTargetSelectTimer)
