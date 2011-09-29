9f5fd3e20fe9696a52169e854a5e6f2f3470b79e
Ruben Motta Mussi de Souza <rubenmussi@hotmail.com>
2011-09-29 03:25:23 -0300

    DB/Conditions: Capture Jormungar Spawn can only be used in Ice Heart Cavern. patch by discover.

---
+++ sql/ChaosCore/world/v5_world_conditions.sql
@@ -0,0 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=17 AND `SourceEntry`=47431;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
+(17,0,47431,0,23,4195,0,0,64,'','Capture Jormungar Spawn can only be used in Ice Heart Cavern');
