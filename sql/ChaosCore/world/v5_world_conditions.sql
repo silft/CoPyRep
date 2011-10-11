508ea7ba5bcc10e3bc1445a27827ce3f256cda88
Ruben Motta Mussi de Souza <rubenmussi@hotmail.com>
2011-10-11 02:53:53 -0300

    DB/lol: Prevent people from summoning an army of gnomes :)

--- sql/ChaosCore/world/v5_world_conditions.sql
+++ sql/ChaosCore/world/v5_world_conditions.sql
@@ -1,3 +1,7 @@
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=17 AND `SourceEntry`=47431;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
 (17,0,47431,0,23,4195,0,0,64,'','Capture Jormungar Spawn can only be used in Ice Heart Cavern');
+
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=17 AND `SourceEntry`=74222;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
+(17,0,74222,0,23,1,0,0,64,'','Gnomish Playbackdevice can only be used in dun morogh');
